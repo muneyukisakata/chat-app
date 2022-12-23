36186ff265ff935d543f649d74a0b04a9c64dba8
坂田宗章 <>
2022-12-23 10:04:01 +0900

    fix chat

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,15 +1,36 @@
 import {
+  Avatar,
+  Box,
   Button,
   chakra,
   Container,
+  Flex,
   Heading,
   Input,
   Spacer,
+  Text,
 } from '@chakra-ui/react'
 
-import { FormEvent, useState } from 'react'
-import { getDatabase, push, ref } from '@firebase/database'
-import { async, FirebaseError } from '@firebase/util'
+import { FormEvent, useEffect, useState } from 'react'
+import { getDatabase, onChildAdded, push, ref } from '@firebase/database'
+import { FirebaseError } from '@firebase/util'
+
+type MessageProps = {
+  message: string
+}
+
+const Message = ({ message }: MessageProps) => {
+  return (
+    <Flex alignItems={'start'}>
+      <Avatar />
+      <Box ml={2}>
+        <Text bgColor={'gray.200'} rounded={'md'} px={2} py={1}>
+          {message}
+        </Text>
+      </Box>
+    </Flex>
+  )
+}
 
 export const Page = () => {
   const [message, setMessage] = useState<string>('')
@@ -30,11 +51,33 @@ export const Page = () => {
       }
     }
   }
+  const [chats, setChats] = useState<{ message: string }[]>([])
+
+  useEffect(() => {
+    try {
+      const db = getDatabase()
+      const dbRef = ref(db, 'chat')
+      return onChildAdded(dbRef, (snapshot) => {
+        const message = String(snapshot.val()['message'] ?? '')
+        setChats((prev) => [...prev, { message }])
+      })
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        console.error(e)
+      }
+    }
+  }, [])
 
   return (
     <Container py={14}>
       <Heading>チャット</Heading>
       <Spacer height={8} aria-hidden />
+      <Flex flexDirection={'column'} overflowY={'auto'} gap={2} height={400}>
+        {chats.map((chat, index) => (
+          <Message message={chat.message} key={`ChatMessage_${index}`} />
+        ))}
+      </Flex>
+
       <chakra.form display={'flex'} gap={2} onSubmit={handleSendMessage}>
         <Input value={message} onChange={(e) => setMessage(e.target.value)} />
         <Button type={'submit'}>送信</Button>
